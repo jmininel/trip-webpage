f8754d4cf5f8f14a22148b2bbb15424428d19112
jmininel <ju.mininel@hotmail.com>
2023-07-09 20:02:57 -0300

    feat: add footer inside layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,27 @@ import './globals.css'
 import { Poppins } from "next/font/google"
 import { NextAuthProvider } from '@/providers/auth'
 import Header from '../components/Header'
+import Footer from '@/components/Footer'
 
-const poppins = Poppins({subsets: ["latin"], weight:['400','500','600','700','800','900']})
+const poppins = Poppins({ subsets: ["latin"], weight: ['400', '500', '600', '700', '800', '900'] })
 
 export const metadata = {
   title: 'FSW Trips',
   description: 'Sistema de reserva de Viagens ',
 }
 
-export default function RootLayout({children,}: {children: React.ReactNode}) {
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
         <NextAuthProvider>
-          <Header/>
+          <Header />
+
           {children}
+
+          <Footer />
         </NextAuthProvider>
-        </body>
+      </body>
     </html>
   )
 }
