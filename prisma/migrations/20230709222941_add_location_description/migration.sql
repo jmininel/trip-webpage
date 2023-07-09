e0540d6f95bec1955a95ce3b7b079fe324d0be5a
jmininel <ju.mininel@hotmail.com>
2023-07-09 19:56:49 -0300

    feat: add migration comit

---
+++ prisma/migrations/20230709222941_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint at repudiandae rerum odit quas reprehenderit fuga nobis labore fugit adipisci face';
